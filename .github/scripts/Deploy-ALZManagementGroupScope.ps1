f87b0ee9947d941156a8bb147c823549d2e23f02
sebastian gredal <segr@anquellion.com>
2023-09-15 10:48:56 +0200

    test

--- .github/scripts/Deploy-ALZManagementGroupScope.ps1
+++ .github/scripts/Deploy-ALZManagementGroupScope.ps1
@@ -23,13 +23,13 @@ if ($WhatIfEnabled) {
   Write-Output 'What-If deployment is enabled. Deployment will not be executed.'
   $result = az deployment mg what-if --name $DeploymentName --location $Location --template-file $TemplateFile --parameters $TemplateParameterFile --verbose 2>&1
   if (!$?) {
-    Write-Error $result
+    Write-Error -Message "$result"
   }
 }
 else {
   Write-Output 'What-If deployment is disabled. Deployment will be executed.'
   az deployment mg create --name $DeploymentName --resource-group $ResourceGroupName --template-file $TemplateFile --parameters $TemplateParameterFile --verbose 2>&1
   if (!$?) {
-    Write-Error $result
+    Write-Error -Message "$result"
   }
 }
