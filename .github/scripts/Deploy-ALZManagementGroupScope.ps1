8f4a32efe59bacfa1e52fdaf3dd35be9f17293db
sebastian gredal <segr@anquellion.com>
2023-09-15 11:19:51 +0200

    add mgmt group id to deployment

--- .github/scripts/Deploy-ALZManagementGroupScope.ps1
+++ .github/scripts/Deploy-ALZManagementGroupScope.ps1
@@ -6,6 +6,9 @@ param (
   [String]$Location = "$($env:LOCATION)",
 
   [Parameter()]
+  [String]$ManagementGroupId,
+
+  [Parameter()]
   [String]$TemplateFile,
 
   [Parameter()]
@@ -21,7 +24,7 @@ $DeploymentName = "$Name-{0}" -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ
 # Create the deployment
 if ($WhatIfEnabled) {
   Write-Output 'What-If deployment is enabled. Deployment will not be executed.'
-  $result = az deployment mg what-if --name $DeploymentName --location $Location --template-file $TemplateFile --parameters $TemplateParameterFile --verbose 2>&1
+  $result = az deployment mg what-if --name $DeploymentName --management-group-id $ManagementGroupId --location $Location --template-file $TemplateFile --parameters $TemplateParameterFile --verbose 2>&1
   if (!$?) {
     Write-Error -Message "$result"
   }
@@ -29,7 +32,7 @@ if ($WhatIfEnabled) {
 }
 else {
   Write-Output 'What-If deployment is disabled. Deployment will be executed.'
-  az deployment mg create --name $DeploymentName --resource-group $ResourceGroupName --template-file $TemplateFile --parameters $TemplateParameterFile --verbose 2>&1
+  az deployment mg create --name $DeploymentName --management-group-id $ManagementGroupId --location $Location --template-file $TemplateFile --parameters $TemplateParameterFile --verbose 2>&1
   if (!$?) {
     Write-Error -Message "$result"
   }
